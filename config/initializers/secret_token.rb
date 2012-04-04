67c4b89c4dd9e9f409419f3c17d68eb34dafc0a9
Andrea Jessup <jessup.andrea@gmail.com>
2012-04-04 18:31:27 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarelyEdible::Application.config.secret_token = '61e61016e37d64463b4ef19b32b5f36d5a386f4ec767e4fe84262fbf23265ddb837c1c3f91f8eb503c96e7bd2a33465b4e8c2f6cbcb267e35cf3dffa2d29246f'
